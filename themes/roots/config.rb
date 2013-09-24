badd90ee4e9ccac6a7ef0bf0d5a78c5ce537250e
Roger Dickey <dickeyr@gmail.com>
2013-09-24 13:58:08 -0500

    Include empty SASS files ready to start customizing the site layout.

---
+++ themes/roots/config.rb
@@ -0,0 +1,11 @@
+sass_dir = "assets/scss"
+css_dir = "assets/css"
+images_dir = "assets/img"
+js_dir = "assets/js"
+relative_assets = true
+project_type = :stand_alone
+environment = :development
+#environment = :production
+output_style = :expanded
+#output_style = :compressed
+
